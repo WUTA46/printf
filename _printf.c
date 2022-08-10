05a0d57d2581e60768052c164df564f4c664267e
Tsegazeab <Tsegazeabwuta@gmail.com>
2022-08-10 09:27:42 -0700

    a function produce output according to format

---
+++ _printf.c
@@ -0,0 +1,51 @@
+#include "main.h"
+
+/**
+ * _printf - prints a formatted string
+ * You don’t have to reproduce the buffer handling of the C library printf function
+ * You don’t have to handle the flag characters
+ * You don’t have to handle field width
+ * You don’t have to handle precision
+ * You don’t have to handle the length modifiers
+ *
+ * @format: a string that is gonna be printed
+ * Return: the number of characters printed or NULL if it fails
+ */
+
+int _printf(const char *format, ...)
+{
+	int (*pfunc)(va_list, flags_t *);
+	const char *p;
+	va_list arguments;
+	flags_t flags = {0, 0, 0};
+
+	register int count = 0;
+
+	va_start(arguments, format);
+	if (!format || (format[0] == '%' && !format[1]))
+		return (-1);
+	if (format[0] == '%' && format[1] == ' ' && !format[2])
+		return (-1);
+	for (p = format; *p; p++)
+	{
+		if (*p == '%')
+		{
+			p++;
+			if (*p == '%')
+			{
+				count += _putchar('%');
+				continue;
+			}
+			while (get_flag(*p, &flags))
+				p++;
+			pfunc = get_print(*p);
+			count += (pfunc) ? pfunc(arguments, &flags) : _printf("%%%c", *p);
+		}
+		else
+			count += _putchar(*p);
+	}
+	_putchar(-1);
+	va_end(arguments);
+	return (count);
+}
+
