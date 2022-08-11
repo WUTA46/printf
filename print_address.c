5b45908340a9f286a910442dd1cc16e6d8166e3b
Tsegazeab <Tsegazeabwuta@gmail.com>
2022-08-11 00:20:09 -0700

    a function that print inputs address in base 16

--- print_address.c
+++ print_address.c
@@ -11,7 +11,7 @@
 int print_address(va_list l, flags_t *f)
 {
 	char *str;
-	unsigned long int p = va_arg(l,unsigned long int);
+	unsigned long int p = va_arg(l, unsigned long int);
 
 	register int count = 0;
 
